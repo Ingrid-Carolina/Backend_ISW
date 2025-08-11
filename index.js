a5ff08b0b277212bbd1646369241f8fc968c4cb7
Carlos Flores <carlosfernandoflores321@gmail.com>
2025-08-11 17:33:55 -0600

    fix: ruta de host

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://pilotosbaseball.netlify.app'],
   credentials: true,
 }));
 
