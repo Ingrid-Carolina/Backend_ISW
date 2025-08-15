8f97107d40fcfbd2efdb3077f65d7d7e9194eebe
Carlos Flores <carlosfernandoflores321@gmail.com>
2025-08-15 16:45:30 -0600

    fix: reactivacion de endpoints, prueba de health pasada

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { dbConnect } from './config/postgre.js'; 
-//import router from './routes/router.js';
+import router from './routes/router.js';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -24,20 +24,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // DB
-//dbConnect(); 
+dbConnect(); 
 
 // Temporal solo para ver los logs del request
+/*
 app.use((req, _res, next) => {
   console.log(`[REQ] ${req.method} ${req.url}`);
   next();
-});
+});*/
 
 // Rutas
 
 app.get('/health', (_req, res) => res.json({ ok: true }));
 app.get('/', (_req, res) => res.type('text/plain').send('API alive'));
 
-//app.use('/auth', router);
+app.use('/auth', router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
