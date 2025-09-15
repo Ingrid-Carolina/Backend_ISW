d04ec13721acf2216803db094a85975631d7d48c
Carlos Flores <carlosfernandoflores321@gmail.com>
2025-09-14 19:39:06 -0600

    asdas

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://pilotosbaseball.netlify.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://pilotosbaseball.netlify.app'],
   credentials: true,
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
 }));
 
 // Cookies HttpOnly
